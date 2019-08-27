c30983c579328522c3d575c5f8cef7a612e8317d
wadha08 <wadha-ra@hotmail.com>
2019-08-27 09:55:15 +0300

    car market updated

--- cars/views.py
+++ cars/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 from .models import Car
+from .forms import CarForm
 
 def car_list(request):
 	cars = Car.objects.all()
@@ -18,15 +19,34 @@ def car_detail(request, car_id):
 
 
 def car_create(request):
-	#Complete Me
-	return render(...)
+	form = CarForm()
+	if request.method == "POST":
+		form = CarForm(request.POST , request.FILES)
+		if form.is_valid():
+			form.save()
+			return redirect('car-list')
+	context = {
+		"form": form,
+	}
+	return render(request, 'create.html' , context)
 
 
 def car_update(request, car_id):
-	#Complete Me
-	return render(...)
+	Car_obj = Car.objects.get(id=car_id)
+	form = CarForm(instance = Car_obj)
+	if request.method == "POST":
+		form = CarForm(request.POST , instance=Car_obj)
+		if form.is_valid():
+			form.save()
+			return redirect('car-list')
+	context = {
+        "Car_obj": Car_obj,
+        "form": form,
+    }
+	return render(request, 'update.html', context)
 
 
 def car_delete(request, car_id):
-	#Complete Me
-	return render(...)
+	Car_obj = Car.objects.get(id=car_id)
+	Car_obj.delete()
+	return redirect('car-list')
